07143ff07d7b17271b73114ff51fc6b8c9a6177f
Anna Escalada Cornellas <anna@Annas-MacBook-Air.local>
2026-03-09 14:07:26 +0100

    feat: organise package and tsconfig

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: './packages/backend/src/modules/**/schema/*.graphql',
+  schema: "./apps/backend/src/modules/**/schema/*.graphql",
   generates: {
     './packages/shared/src/graphql/types.ts': {
       plugins: ['typescript', 'typescript-resolvers'],
